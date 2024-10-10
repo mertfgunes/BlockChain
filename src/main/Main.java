1a407c49c0c20438a006cc9ece8c1c4e174f43ff
Mert <mertfaruk.ie@gmail.com>
2024-10-10 11:02:26 +0100

    Signatures has been created

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,15 @@
 package main;
 
+import java.util.Arrays;
+
 public class Main {
-	public static void main (String[] args ) {
-		System.out.println("First Commit");
+
+	public static void main(String[] args) {
+		String[] list1 = {"a", "b", "c" };
+		String[] list2 = {"a", "b", "c" };
+		
+		
+		System.out.println(Arrays.hashCode(list1));
+		System.out.println(Arrays.hashCode(list2));	
 	}
 }
