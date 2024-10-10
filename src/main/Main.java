e98358a2b5857e3c844554c888800b438ae54aa2
Mert <mertfaruk.ie@gmail.com>
2024-10-10 11:02:58 +0100

    Made changes on signature to see the difference

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		String[] list1 = {"a", "b", "c" };
-		String[] list2 = {"a", "b", "c" };
+		String[] list2 = {"aa", "b", "c" };
 		
 		
 		System.out.println(Arrays.hashCode(list1));
