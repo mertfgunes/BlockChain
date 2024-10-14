8c575914eddbf32e25aa0cc4e3f106651e42bc58
Mert <mertfaruk.ie@gmail.com>
2024-10-14 19:54:12 +0100

    The class block has been created.
    Finished the block.

---
+++ src/main/Block.java
@@ -0,0 +1,25 @@
+package main;
+
+import java.util.Arrays;
+
+public class Block {
+	private int prevHash;
+	private String[] transaction;
+	
+	private int blockHash;
+	
+	public Block(int prevHash, String[] transaction) {
+		this.prevHash = prevHash;
+		this.transaction = transaction;
+		
+		Object [] contens = {Arrays.hashCode(transaction), prevHash};
+		this.blockHash = Arrays.hashCode(contens);
+	}
+	public int getPrevHas() {
+		return prevHash;
+	}
+	public String[]  getTransaction () {
+		return transaction;
+	}
+	
+}
